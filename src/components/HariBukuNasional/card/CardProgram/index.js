aa3e38e24a8e5e502052cee54bbcbab02f762330
AldianBaim <aldiansyahibrahim5@gmail.com>
2024-05-18 18:07:20 +0700

    fix: set width image

--- src/components/HariBukuNasional/card/CardProgram/index.js
+++ src/components/HariBukuNasional/card/CardProgram/index.js
@@ -5,7 +5,7 @@ const CardProgram = ({backgroundColor, image, title, description}) => {
     return (
         <section className={`card-body border-0 card-program p-4 ${backgroundColor}`}>
             <div className="d-flex flex-column flex-lg-row align-items-center gap-4">
-                <img src={`/assets/image/hari-buku-nasional/${image}`} alt="Card 1" />
+                <img src={`/assets/image/hari-buku-nasional/${image}`} width="100%" alt="Card 1" />
                 <div>
                     <h2 className="mb-3">{title}</h2>
                     <p className="lead">{description}</p>
