b1608ebcc0cc4c88d4c689beb85b238e42d8b2bc
Ahmad Oriza <ahmadoriza@gmail.com>
2024-05-19 23:35:27 +0700

    Pasang menu Sastra di laman utama

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -164,11 +164,13 @@ const Navbar = ({ nightMode }) => {
                                     </li>
                                 </ul>
                             </li>
-                            {/* <li className="nav-item mx-1">
-                                <a target="_blank" rel="noreferrer" href="https://pusbuk.kemdikbud.go.id" className={({ isActive }) => isActive ? 'nav-link fw-bold' : 'nav-link'} aria-current="page">Profil</a>
-                            </li> */}
                             <li className="nav-item mx-1">
                                 <div onClick={() => window.location.replace("https://pusbuk.kemdikbud.go.id")} className="nav-link" style={{ cursor: 'pointer' }}>Profil</div>
+                            </li>
+                            <li className="nav-item mx-1">
+                                <Link className="nav-link" to="/sastra-masuk-kurikulum">
+                                    Sastra Masuk Kurikulum <sup class="text-danger">Baru</sup>
+                                </Link>
                             </li>
                             {
                                 token && (
