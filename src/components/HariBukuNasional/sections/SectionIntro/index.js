191fa7759bc20922131a4f633faa79b74f5c4ed9
Ahmad Oriza <ahmadoriza@gmail.com>
2024-05-20 13:37:11 +0700

    Change youtube video

--- src/components/HariBukuNasional/sections/SectionIntro/index.js
+++ src/components/HariBukuNasional/sections/SectionIntro/index.js
@@ -7,7 +7,7 @@ const SectionIntro = () => {
                         <h1 className="mb-4 display-5">Apa itu Program <br /> “Sastra Masuk Kurikulum”?</h1>
                         <p>Program “Sastra Masuk Kurikulum” adalah turunan dari program Episode Merdeka Belajar 15: Kurikulum <br /> Merdeka dan Platform Merdeka Mengajar. Program ini dirancang untuk memperkenalkan siswa pada <br /> beragam karya sastra dari berbagai budaya dan periode waktu.</p>
                         <div class="embed-responsive embed-responsive-16by9 w-100" style={{ marginTop: '100px' }}>
-                            <iframe class="embed-responsive-item w-100" height={"500"} style={{ borderRadius: '20px' }} src="https://www.youtube.com/embed/RTzTrh_5KVI?rel=0" allowfullscreen></iframe>
+                            <iframe class="embed-responsive-item w-100" height={"500"} style={{ borderRadius: '20px' }} src="https://www.youtube.com/embed/Jvwj-A0XAqI?rel=0" allowfullscreen></iframe>
                         </div>
                     </div>
                 </div>
