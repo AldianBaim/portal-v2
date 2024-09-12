4a02b30e06a6d576b19b885a3ecdc070a07cd07e
AldianBaim <aldiansyahibrahim5@gmail.com>
2024-09-12 10:17:43 +0700

    feat: cache halaman panduan get summary

--- src/pages/guide/forTeacher.js
+++ src/pages/guide/forTeacher.js
@@ -6,21 +6,26 @@ import SectionIntroTeacher from '../../components/guide/sections/SectionIntro/Se
 import SectionJelajahi from '../../components/guide/sections/SectionJelajahi/SectionJelajahi'
 import SectionStats from '../../components/home/sections/SectionStats/SectionStats'
 import { BASE_URL } from '../../utils/config'
+import useSWR from 'swr';
+
+const fetcher = (url) => fetch(url).then((res) => res.json());
+
+function useStatisticBook() {
+    const { data, error, isValidating } = useSWR(`${BASE_URL}/api/statistic/getSummary`, fetcher, {
+        refreshInterval: 18000000,    // Re-fetch setiap 5 Jam
+        dedupingInterval: 18000000,   // Cegah fetching ulang dalam 5 Jam
+        revalidateOnFocus: false,  // Jangan revalidate saat window fokus kembali
+        revalidateOnReconnect: false
+    });
+
+    const statisticBook = data ?? {};
+    const loadingStatisticBook = isValidating ?? false;
+  
+    return { statisticBook, errorStatisticBook: error ,loadingStatisticBook };
+}
 
 const ForTeacher = () => {
-    const [statisticBook, setStatisticBook] = useState([])
-
-    useEffect(() => {
-        const getStatisticBook = async () => {
-            try {
-                const response = await axios.get(`${BASE_URL}/api/statistic/getSummary`);
-                setStatisticBook(response.data)
-            } catch (error) {
-                return error.message
-            }
-        }
-        getStatisticBook();
-    }, [])
+    const { statisticBook } = useStatisticBook();
 
     const content = {
         subtitle: 'BUKU UNTUK GURU',
