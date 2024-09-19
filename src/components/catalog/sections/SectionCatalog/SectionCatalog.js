a07f2cea97819c2eb8bf488614aa16c9afca4513
AldianBaim <aldiansyahibrahim5@gmail.com>
2024-09-19 20:50:52 +0700

    feat: target blank for link book detail on card

--- src/components/catalog/sections/SectionCatalog/SectionCatalog.js
+++ src/components/catalog/sections/SectionCatalog/SectionCatalog.js
@@ -357,14 +357,14 @@ const SectionCatalog = ({ level, setLevelNonText, tag, setTagNonText, setLatestB
                                         : currentData?.map((book, index) => {
                                             return (
                                                 <div key={index} className="col-lg-4 my-2">
-                                                    <Link key={index} to={`/katalog/${book.slug}`} className="text-decoration-none text-dark">
+                                                    <a href={`/katalog/${book.slug}`} target="_blank" className="text-decoration-none text-dark">
                                                         <CardBook
                                                             image={book.image}
                                                             title={book.title}
                                                             typeBook={book.type}
                                                             level={book.level}
                                                         />
-                                                    </Link>
+                                                    </a>
                                                 </div>
                                             )
                                         })
