8ed02f1c5a61c712309e3a98338de8c63b6bad79
AldianBaim <aldiansyahibrahim5@gmail.com>
2025-02-24 14:07:51 +0700

    fix: change text

--- src/components/guide/sections/SectionIntro/SectionIntroTeacher.js
+++ src/components/guide/sections/SectionIntro/SectionIntroTeacher.js
@@ -8,7 +8,7 @@ const SectionIntroTeacher = () => {
                     </div>
                     <div className="col-lg-6">
                         <h2 className="text-blue mb-3 fw-bold">Akses gratis buku <br /> pelajaran resmi</h2>
-                        <p>Dapatkan akses buku, buku teks pelajaran maupun buku non teks, langsung dari Pusat Perbukuan, Kemdikbud.</p>
+                        <p>Dapatkan akses buku, buku teks pelajaran maupun buku non teks, langsung dari Pusat Perbukuan, Kemendikdasmen.</p>
                         <p>Bapak/ibu guru dapat mengakses buku dalam berbagai format seperti PDF, audio, dan interaktif.</p>
                         <p>Dapatkan "update" buku teks pelajaran, berdasarkan kurikulum yang berlaku, buku Kurikulum 2013 (K-13) dan Kurikulum Merdeka.</p>
                     </div>
