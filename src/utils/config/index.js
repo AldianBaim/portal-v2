8a5be2f8b33f5245f5f2f9e0eddfbcb0420b95f0
Oriza <ahmadoriza@gmail.com>
2025-03-15 11:25:20 +0700

    Fix base url

--- src/utils/config/index.js
+++ src/utils/config/index.js
@@ -1,3 +1,4 @@
 // API BASE URL
 // export const BASE_URL = 'https://api.buku.kemdikbud.go.id';
 export const BASE_URL = 'https://api.buku.cloudapp.web.id';
+export const BASE_URL_PUSDATIN = 'https://api.buku.cloudapp.web.id';
